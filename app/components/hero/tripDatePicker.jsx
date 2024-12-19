68fdc290fddadd39ab462af049faca9b5042fdeb
Hady Awny <hadyawny5@gmail.com>
2024-12-19 22:35:35 +0200

    fixed hero and datepicker

--- app/components/hero/tripDatePicker.jsx
+++ app/components/hero/tripDatePicker.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import "@/app/components/hero/datepickerStyles.css";
 export default function TripDatePicker() {
   const [isRoundTrip, setIsRoundTrip] = useState(true);
   const [dateStart, setDateStart] = useState();
@@ -40,7 +40,7 @@ export default function TripDatePicker() {
   }
 
   return (
-    <Popover className="h-8 ">
+    <Popover className="relative h-8 ">
       <PopoverButton className="flex items-center justify-start border border-grey-200 py-2 pl-4 pr-16 w-60 cursor-pointer ">
         <Image
           src="/calendaricon.png"
@@ -52,34 +52,52 @@ export default function TripDatePicker() {
 
         {title}
       </PopoverButton>
-      <PopoverPanel anchor="bottom" className=" bg-trueWhite px-6 pt-6 pb-2  border border-grey-200 shadow-lg rounded-md  " >
+      <PopoverPanel
+        anchor={""}
+        className="flex flex-col items-center bg-trueWhite  pt-4 pb-2  border border-grey-200 shadow-lg rounded-md absolute  z-10  "
+        style={{ width:"600px", top: "-50%",left: "-90%",   }} 
+      >
         <div className="mb-4 flex justify-between items-center mx-2">
           <div className="m">
-          <input
-            type="radio"
-            name="round"
-            id="round"
-            onChange={() => setIsRoundTrip(true)}
-            checked={isRoundTrip}
-            className="mr-2 "
-            
-          />
-          <label htmlFor="round" className="mr-4">Round Trip</label>
-          <input
-            type="radio"
-            name="round"
-            id="oneWay"
-            checked={!isRoundTrip}
-            onChange={() => setIsRoundTrip(false)}
-            className="mr-2"
-          />
-          <label htmlFor="oneWay" className="mr-4">One-Way</label>
+            <input
+              type="radio"
+              name="round"
+              id="round"
+              onChange={() => setIsRoundTrip(true)}
+              checked={isRoundTrip}
+              className="mr-2 "
+            />
+            <label htmlFor="round" className="mr-4">
+              Round Trip
+            </label>
+            <input
+              type="radio"
+              name="round"
+              id="oneWay"
+              checked={!isRoundTrip}
+              onChange={() => setIsRoundTrip(false)}
+              className="mr-2"
+            />
+            <label htmlFor="oneWay" className="mr-2">
+              One-Way
+            </label>
           </div>
-          <PopoverButton className="ml-4 px-4 py-2 bg-blue-500 bg-purpleBlue text-trueWhite rounded">
+          <div className="flex items-center justify-start border border-grey-200 py-2 pl-4 pr-16 w-60 rounded ">
+            <Image
+              src="/calendaricon.png"
+              alt="dropdown icon"
+              width={24}
+              height={24}
+              className="object-contain mr-4"
+            />
+
+            {title}
+          </div>
+          <PopoverButton className="ml-2 px-4 py-2 bg-blue-500 bg-purpleBlue text-trueWhite rounded">
             Done
           </PopoverButton>
         </div>
-
+        
         <DatePicker
           placeholderText={isRoundTrip ? "Depart - Arrive" : "Select Date"}
           selectsRange={isRoundTrip}
@@ -102,7 +120,7 @@ export default function TripDatePicker() {
           dateFormat="dd MMM yyyy"
           monthsShown={2}
           inline
-          
+          formatWeekDay={(day) => day.substring(0, 1)}
         />
       </PopoverPanel>
     </Popover>
