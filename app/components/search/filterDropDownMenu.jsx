fce1cb7de72a520f1db21804bdb11357da8cc070
Hady Awny <hadyawny5@gmail.com>
2024-12-29 12:52:49 +0200

    added filter Drop menu

---
+++ app/components/search/filterDropDownMenu.jsx
@@ -0,0 +1,48 @@
+"use client"
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function FilterDropDownMenu({ title, icon, data ,onItemsChange}) {
+  const [selectedTitle, setSelectedTitle] = useState(title);
+
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <MenuButton
+        as="div"
+        className="flex items-center justify-start border border-grey-200 py-2 pl-4 w-64 cursor-pointer "
+      >
+        <Image
+          src={icon}
+          alt="dropdown icon"
+          width={24}
+          height={24}
+          className="object-contain mr-4"
+        />
+        {selectedTitle}
+      </MenuButton>
+      <MenuItems
+        className="absolute left-0 mt-2 bg-white border border-grey-200 shadow-lg rounded-md max-h-56 overflow-y-auto w-[var(--button-width)] p-4"
+      >
+        {data?.map((item) => (
+          <MenuItem key={item._id}>
+            {() => (
+              <p
+                onClick={() => {
+                  setSelectedTitle(item.code);
+                  onItemsChange({ item: item.code });
+                }}
+                className={`block px-4 py-2 
+                   "bg-blue-100" 
+                 cursor-pointer`}
+              >
+                {item.code}
+              </p>
+            )}
+          </MenuItem>
+        ))}
+      </MenuItems>
+    </Menu>
+  );
+}
