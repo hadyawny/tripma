df8b56c056976db618943462bac324fb64085125
Hady Awny <hadyawny5@gmail.com>
2024-12-31 12:39:55 +0200

    fix

--- app/components/loadingCircle.jsx
+++ app/components/loadingCircle.jsx
@@ -1,12 +1,11 @@
-"use client"
+"use client";
 
 export const LoadingCircle = () => {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="w-12 h-12 border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
-      </div>
-    );
-  };
-  
-  export default LoadingCircle;
-  
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-12 h-12 border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default LoadingCircle;
